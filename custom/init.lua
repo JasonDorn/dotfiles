9a452b779962c7e2d80a815e0c71476557eb42c6
Jason Dorn <apollo701@gmail.com>
2022-12-23 11:33:28 -0500

    adds LSPs and formatters

--- custom/init.lua
+++ custom/init.lua
@@ -1,11 +1,9 @@
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
 -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
 
 local opt = vim.opt
 local g = vim.g
 
 g.noswapfile = true
-g.test.strategy = 'neovim'
 
 opt.tabstop = 4
 opt.softtabstop = 4
--- custom/plugins/init.lua
+++ custom/plugins/init.lua
@@ -1,13 +1,47 @@
 return {
-    ["goolord/alpha-nvim"] = {
-        after = "base46",
-        disable = false,
-        config = function()
-            require "plugins.configs.alpha"
-        end,
-    },
-    ["nvim-telescope/telescope-fzf-native.nvim"] = {
-        run = "make"
-    },
-    ["vim-test/vim-test"] = {},
+	["goolord/alpha-nvim"] = {
+		after = "base46",
+		disable = false,
+		config = function()
+			require("plugins.configs.alpha")
+		end,
+	},
+	["nvim-telescope/telescope-fzf-native.nvim"] = {
+		run = "make",
+	},
+	["vim-test/vim-test"] = {},
+	["kristijanhusak/vim-create-pr"] = {},
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls")
+		end,
+	},
+	["neovim/nvim-lspconfig"] = {
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.plugins.lspconfig")
+		end,
+	},
+	["williamboman/mason.nvim"] = {
+		override_options = {
+			ensure_installed = {
+				"lua-language-server",
+				"stylua",
+				"rubocop",
+
+				-- web dev
+				"css-lsp",
+				"html-lsp",
+				"typescript-language-server",
+				"deno",
+				"emmet-ls",
+				"json-lsp",
+
+				-- shell
+				"shfmt",
+				"shellcheck",
+			},
+		},
+	},
 }
