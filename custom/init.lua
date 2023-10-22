fde84eef575eab8172f0defec6464368e73130e8
Jason Dorn <apollo701@gmail.com>
2023-10-22 14:45:54 -0400

    add autoformatting on save

--- custom/init.lua
+++ custom/init.lua
@@ -1,3 +1,13 @@
+local opt = vim.opt
+
+opt.swapfile = false
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.shiftround = false
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
 -- local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
