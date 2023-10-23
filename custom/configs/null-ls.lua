023fbee26f2395e8c21e9e511c952ef90862798c
Jason Dorn <apollo701@gmail.com>
2023-10-23 11:48:48 -0400

    refactor null-ls and add global git ignore

--- custom/configs/null-ls.lua
+++ custom/configs/null-ls.lua
@@ -1,56 +1,57 @@
 local null_ls = require("null-ls")
 
-local b = null_ls.builtins
+local formatting = null_ls.builtins.formatting
+local lint = null_ls.builtins.diagnostics
 
 local sources = {
 
 	-- webdev stuff
-	b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-	b.diagnostics.deno_lint,
-	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
+	formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+	lint.deno_lint,
+	formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
 
 	-- Lua
-	b.formatting.stylua,
+	formatting.stylua,
 
 	-- cpp
-	b.formatting.clang_format,
+	formatting.clang_format,
 
 	-- ruby
-	b.formatting.standardrb,
-	b.diagnostics.standardrb,
+	formatting.standardrb,
+	lint.standardrb,
 	-- b.formatting.rubocop,
 	-- b.diagnostics.rubocop,
 }
 
 local async_formatting = function(bufnr)
-    bufnr = bufnr or vim.api.nvim_get_current_buf()
+	bufnr = bufnr or vim.api.nvim_get_current_buf()
 
-    vim.lsp.buf_request(
-        bufnr,
-        "textDocument/formatting",
-        vim.lsp.util.make_formatting_params({}),
-        function(err, res, ctx)
-            if err then
-                local err_msg = type(err) == "string" and err or err.message
-                -- you can modify the log message / level (or ignore it completely)
-                vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-                return
-            end
+	vim.lsp.buf_request(
+		bufnr,
+		"textDocument/formatting",
+		vim.lsp.util.make_formatting_params({}),
+		function(err, res, ctx)
+			if err then
+				local err_msg = type(err) == "string" and err or err.message
+				-- you can modify the log message / level (or ignore it completely)
+				vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+				return
+			end
 
-            -- don't apply results if buffer is unloaded or has been modified
-            if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-                return
-            end
+			-- don't apply results if buffer is unloaded or has been modified
+			if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+				return
+			end
 
-            if res then
-                local client = vim.lsp.get_client_by_id(ctx.client_id)
-                vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-                vim.api.nvim_buf_call(bufnr, function()
-                    vim.cmd("silent noautocmd update")
-                end)
-            end
-        end
-    )
+			if res then
+				local client = vim.lsp.get_client_by_id(ctx.client_id)
+				vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+				vim.api.nvim_buf_call(bufnr, function()
+					vim.cmd("silent noautocmd update")
+				end)
+			end
+		end
+	)
 end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
