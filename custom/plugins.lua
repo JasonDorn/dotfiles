88a847bdc98a99b3344642666d4be9dcdb8a0d17
Jason Dorn <apollo701@gmail.com>
2023-10-23 21:00:13 -0400

    refactor other nvim

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -97,60 +97,7 @@ local plugins = {
 		"rgroli/other.nvim",
 		ft = "ruby",
 		config = function()
-			require("other-nvim").setup({
-				mappings = {
-					-- builtin mappings
-					"rails",
-					-- custom mapping
-					{
-						pattern = "/(.*)/(.*)/.*.jsx$",
-						target = {
-							{
-								target = "/%1/%2/%2.test.jsx",
-								context = "test",
-							},
-							{
-								target = "/%1/%2/%2.test.js",
-								context = "test",
-							},
-						},
-					},
-					{
-						pattern = "/(.*)/(.*)/.*.js$",
-						target = {
-							{
-								target = "/%1/%2/%2.test.jsx",
-								context = "test",
-							},
-							{
-								target = "/%1/%2/%2.test.js",
-								context = "test",
-							},
-						},
-					},
-				},
-				transformers = {
-					-- defining a custom transformer
-					lowercase = function(inputString)
-						return inputString:lower()
-					end,
-				},
-				style = {
-					-- How the plugin paints its window borders
-					-- Allowed values are none, single, double, rounded, solid and shadow
-					border = "solid",
-
-					-- Column seperator for the window
-					seperator = "|",
-
-					-- width of the window in percent. e.g. 0.5 is 50%, 1.0 is 100%
-					width = 0.7,
-
-					-- min height in rows.
-					-- when more columns are needed this value is extended automatically
-					minHeight = 2,
-				},
-			})
+			require("custom.configs.other")
 		end,
 	},
 
